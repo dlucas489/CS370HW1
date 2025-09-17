544d289752c7567126ea22b3318a5db21520e0af
dlucas489 <115660983+dlucas489@users.noreply.github.com>
2025-09-17 16:09:18 -0600

    Update Driver.c from Hello World to HW1 spec

--- Driver.c
+++ Driver.c
@@ -1,8 +1,22 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <stdlib.h> // for atoi
+#include "MemoryManager.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        // Guard may not be necessary? Spec says we can assume correct # of input args...
+        fprintf(stderr, "Usage: %s <seed>\n", argv[0]);
+        return 1;
+    }
 
-    printf("Hello, World!\n");
+    int seed = atoi(argv[1]);
+    srand(seed);
+
+    printf("[Driver] With seed: %d\n", seed);
+
+    float running_ratio = get_running_ratio();
+
+    printf("[Driver] AVG perfect_sqr/non_perfect_sr ratio: %.6f\n", running_ratio);
 
     return 0;
 }
